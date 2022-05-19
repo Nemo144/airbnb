cd85cef4a27156b6a087d9c182483a1c72e3ef1d
Nnamdi Okeke <onnamdialex@gmail.com>
2022-05-20 00:23:12 +0100

    :rocket: navbar and hero component done?

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,18 @@ import { images } from "../constants";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <img src={images.group77} alt="hero" />
-        <h1>Online Experience</h1>
-        <p>
-          Join unique interactive activities led by one-of-a-kind hosts—all
-          without leaving home.
-        </p>
+    <div className="flex flex-col justify-start items-start">
+      <div className="flex justify-center items-center pt-5 pl-56">
+        <img src={images.group77} alt="hero" className="" />
       </div>
-    </section>
+      <div className="flex flex-col pl-20 pt-10">
+        <div className="text-2xl pb-5">
+          <h1>Online Experience</h1>
+        </div>
+        <p>Join unique interactive activities led by</p>
+        <p>one-of-a-kind hosts—all without leaving</p>
+        <p>home.</p>
+      </div>
+    </div>
   );
 };
